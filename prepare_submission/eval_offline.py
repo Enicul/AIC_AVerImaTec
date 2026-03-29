377c0cbb912c8c18ba25c0a6224415694f06aedf
Herbert Ullrich <ullriher@fel.cvut.cz>
2026-03-29 16:16:32 +0200

    eval offline switch to dev set

--- prepare_submission/eval_offline.py
+++ prepare_submission/eval_offline.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
 
     print("Root dir:", args.root_dir)
     # p2_data = load_json(os.path.join(args.root_dir, "data/data_clean/split_data/test.json"))
-    p2_data = load_json(os.path.join(args.root_dir, "test.json"))
+    p2_data = load_json(os.path.join(args.root_dir, "val.json"))
     if len(args.pred_file_path) > 0:
         pred_file = load_json(args.pred_file_path)
     else:
@@ -84,10 +84,10 @@ if __name__ == "__main__":
             )
         )
 
-    if os.path.exists(os.path.join(args.root_dir, "prepare_submission/converted_results", "gt_evid_test.json")):
+    if os.path.exists(os.path.join(args.root_dir, "prepare_submission/converted_results", "gt_evid.json")):
         gt_evid_flag = True
         gt_evid_set = load_json(
-            os.path.join(args.root_dir, "prepare_submission/converted_results", "gt_evid_test.json")
+            os.path.join(args.root_dir, "prepare_submission/converted_results", "gt_evid.json")
         )
     else:
         gt_evid_flag = False
@@ -110,6 +110,8 @@ if __name__ == "__main__":
     else:
         all_eval_results = []
     for i, row in enumerate(p2_data):
+        if i < len(all_eval_results):
+            continue
         if args.debug and i > 4:
             break
         if i % 20 == 0:
@@ -131,10 +133,10 @@ if __name__ == "__main__":
         gt_verdict = row["label"]
         if gt_evid_flag:
             gt_evid = gt_evid_set[i]
-            print(i)
         else:
             gt_evid = [convert_qa_format(qa, mllm, mllm_name, args.root_dir) for qa in row["questions"]]
             gt_evid_set.append(gt_evid)
+
         pred_evid = pred_file[i]["evidence"]
         pred_justi = pred_file[i]["justification"]
         pred_questions = pred_file[i]["questions"]
@@ -145,28 +147,26 @@ if __name__ == "__main__":
             verdict_acc = 1.0
         else:
             verdict_acc = 0.0
-            
-        if False:
-            # evidence evaluation
-            detailed_evid_val, evid_val_score = val_evid_idv(mllm, mllm_name, pred_evid, gt_evid, False, True)
-            img_scores = compute_image_scores(mllm, mllm_name, pred_evid, gt_evid, evid_val_score)
-            _, evid_acc, _ = utils.get_auto_recall(detailed_evid_val, img_scores, len(gt_evid), len(pred_evid))
-            # justification generation
-            justi_feedback, justi_score = textual_val_single(
-                gt_justification, pred_justi, args.root_dir, mllm_name, mllm, "justification", args.debug
-            )
-            justi_acc = utils.justi_recall_compute(justi_feedback, justi_score)
-            # question generation
-            ques_feedback, ques_score = textual_val_single(
-                gt_questions, pred_questions, args.root_dir, mllm_name, mllm, "question", args.debug
-            )
-            ques_acc = utils.ques_recall_compute(ques_score, len(gt_questions), len(pred_questions))
-            if args.debug:
-                print("##Question:\n", ques_feedback, "\n", ques_score, "\n\t", ques_acc)
-                print("##Verdict:\n", pred_verdict, gt_verdict, verdict_acc)
-                print("##Evidence:\n", detailed_evid_val, "\n", img_scores, "\n\t", evid_acc)
-                print("##Justification:\n", justi_feedback, "\n", justi_score, "\n\t", justi_acc)
-        ques_score = evid_acc = verdict_acc = justi_acc = ques_feedback=ques_score = justi_feedback =justi_score = detailed_evid_val = img_scores = evid_val_score = ques_acc = 0
+        # evidence evaluation
+        detailed_evid_val, evid_val_score = val_evid_idv(mllm, mllm_name, pred_evid, gt_evid, False, True)
+        img_scores = compute_image_scores(mllm, mllm_name, pred_evid, gt_evid, evid_val_score)
+        _, evid_acc, _ = utils.get_auto_recall(detailed_evid_val, img_scores, len(gt_evid), len(pred_evid))
+        # justification generation
+        justi_feedback, justi_score = textual_val_single(
+            gt_justification, pred_justi, args.root_dir, mllm_name, mllm, "justification", args.debug
+        )
+        justi_acc = utils.justi_recall_compute(justi_feedback, justi_score)
+        # question generation
+        ques_feedback, ques_score = textual_val_single(
+            gt_questions, pred_questions, args.root_dir, mllm_name, mllm, "question", args.debug
+        )
+        ques_acc = utils.ques_recall_compute(ques_score, len(gt_questions), len(pred_questions))
+        if args.debug:
+            print("##Question:\n", ques_feedback, "\n", ques_score, "\n\t", ques_acc)
+            print("##Verdict:\n", pred_verdict, gt_verdict, verdict_acc)
+            print("##Evidence:\n", detailed_evid_val, "\n", img_scores, "\n\t", evid_acc)
+            print("##Justification:\n", justi_feedback, "\n", justi_score, "\n\t", justi_acc)
+
         all_eval_results.append(
             {
                 "ques_score": ques_acc,
@@ -199,5 +199,5 @@ if __name__ == "__main__":
     if gt_evid_flag == False:
         json.dump(
             gt_evid_set,
-            open(os.path.join(args.root_dir, "prepare_submission/converted_results", "gt_evid_test.json"), "w"),
+            open(os.path.join(args.root_dir, "prepare_submission/converted_results", "gt_evid.json"), "w"),
         )
